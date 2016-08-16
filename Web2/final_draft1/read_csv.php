7955cd9eaf160948318dbc64930a31ca8dfff84a
branchvincent <branchevincent@gmail.com>
2016-08-15 23:48:04 -0400

    Removed old files

--- Web2/final_draft1/read_csv.php
+++
@@ -1,8 +0,0 @@
-<?php
-	
-	require_once('header.php');
-	require_once('graph_engineer.php');
-	require_once('footer.php');
-	
-?>
-
--- Web2/final_draft1/replications_send.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-	session_start();
-	$rep=(int)($_POST ["replications"]);
-	$_SESSION['replications']=$rep;
-	include('index.php');
-?>
